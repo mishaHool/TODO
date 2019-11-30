a9e14651a224c3306391eb5997c969c5bf4c258b
Miha <vinemaster228@gmail.com>
2019-11-30 17:20:38 +0200

    edit

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ let titlee = document.getElementsByClassName('tittle')[0];
 let deesc = document.getElementsByClassName('desc')[0];
 let prio = document.getElementsByClassName('prior')[0];
 let tasks = document.getElementById('tasks');
+let update = document.getElementsByClassName('update')[0];
 let triTochki;
 let DoNe;
 let DeL;
-
-
-
-
-
+let ddone;
+let cs;
+let EdIt;
+let itle;
+let escip;
+let rio;
 
 class fileSystem{
     constructor(){
@@ -22,162 +24,207 @@ class fileSystem{
    addTask(title, desc, prior){
     this.refresh();
     let data = Date.now();
-    this.tasks
     this.taskss.push({
        index: data,
-       title: title.value,
-       desc: desc.value,
-       prior: prior.value,
+       title: titlee.value,
+       desc: deesc.value,
+       prior: prio.value,
        done: false,
-       doneClicked: false,
-    })
+    });
+    console.log(this.taskss)
    }
-   changeTask(title, desc, prior, index){
-    console.log("consol")
+   changeTask(index){
+        let i;
+    for(i=0;i<this.taskss.length;i++){
+        let tass = this.taskss[i];
+        if(tass.index == index){
+            itle = tass.title;
+            escip = tass.desc;
+            rio = tass.prior;
+            menu.style.display = "flex";
+            titlee.value = itle;
+            deesc.value = escip;
+            prio.value = rio;
+            this.removeTask(index);
+            update.addEventListener('click', function() {
+                tass.title = itle;
+                tass.desc = escip;
+                tass.prior = rio;
+                menu.style.display = 'none';
+                file.addTask(file.taskss);
+                showTasks(file.taskss);
+                clearData()
+            });
+        }
+    }
    }
    removeTask(index){
-    index.remove();
+    for(i=0; i<this.taskss.length;i++){
+        let tass = this.taskss[i];
+        if(tass.index == index){
+            this.taskss.splice(i,1);
+            showTasks(this.taskss)
+        }
+    }
    }
    refresh(){
-       let cs = document.getElementById('css')
+       let cs = document.getElementById('css');
        if(cs != null)  tasks.removeChild(cs)
    }
-   doneTask(index, obj){
-       if(obj.done == true){
-    index.classList.add('backgroundGray')
-       }else{
-           console.log(obj);
-       }
-       if(obj.doneClicked == true) index.classList.add('backgroundGray')
+   doneTask(index){
+       for(i=0; i<this.taskss.length; i++){
+           let tass = this.taskss[i];
+           if(tass.index == index){
+               tass.done = true;
+               showTasks(this.taskss)
+            }
        }
    }
+}
 let file = new fileSystem();
-
-
-
-
-
 
 createOne.addEventListener("click", function(){
     menu.style.display = "flex";
+    if(update.classList.contains('removed') == false){
+        update.classList.add('removed')
+    }
+    if(apply.classList.contains('removed')){
+        apply.classList.toggle('removed')
+    }
 });
 cancel.addEventListener('click', function(){
-    clearData()
+    clearData();
     menu.style.display = "none";
-})
+});
 function clearData(){
     titlee.value = '';
     deesc.value = ' ';
 }
+
+
 apply.addEventListener('click', function(){
-    let ar = file.addTask(titlee, deesc, prio)
+    file.addTask(titlee, deesc, prio);
+
     clearData();
     menu.style.display = "none";
     showTasks(file.taskss);
-    DeL = document.getElementsByClassName('del')
-    DoNe = document.getElementsByClassName('done')
+
+
+    });
+
+function addClicksToMenu(){
+    DeL = document.getElementsByClassName('del');
+    DoNe = document.getElementsByClassName('done');
     triTochki = document.getElementsByClassName("tri-tochki");
+    EdIt = document.getElementsByClassName('edit');
+
     for(i=0; i<triTochki.length; i++){
         let a = triTochki[i];
         a.addEventListener('click', function(e){
             e.target.nextElementSibling.classList.toggle('displayBlock');
         })
-    }    
+    }
     for(i=0; i<DoNe.length; i++){
-    let aa = DoNe[i];    
-    console.log(aa);
-    aa.addEventListener('click', function(){
-        let aaa = aa.parentNode.parentNode.parentNode.parentNode;
-        if(aa.done == true){
-        file.doneTask(aaa, aa);
-        }else{
-        aa.done = true;
-        file.doneTask(aaa, aa)
-        }
-
-        })
+    let el = DoNe[i];
+    el.addEventListener('click', function(){
+        let ind = el.getAttribute('data-index');
+        file.doneTask(ind)
+       })
     }
     for(i=0; i<DeL.length; i++){
         let b = DeL[i];
-        console.log(b)
         b.addEventListener('click', function(){
-            let bb = b.parentNode.parentNode.parentNode.parentNode;
-            file.removeTask(bb)
+            let ind = b.getAttribute('data-index');
+            file.removeTask(ind)
         })
     }
-    })
-
+    for(i=0; i<EdIt.length; i++){
+        let bb = EdIt[i];
+        bb.addEventListener('click', function(){
+            let ind = bb.getAttribute('data-index');
+            if(update.classList.contains('removed')){
+                update.classList.toggle('removed')
+            }
+            if(apply.classList.contains('removed') == false){
+                apply.classList.add('removed')
+            }
+            file.changeTask(ind)
+           })
+        }
+}
 function showTasks(arr){
-    
     let b = document.getElementById('css');
-    if(b != null)   b.remove()
-    let cs = document.createElement('div');
-    cs.id = "css"
-    
-    for(i=0; i!=arr.length; i++){
+    if(b != null)   b.remove();
+    cs = document.createElement('div');
+    cs.id = "css";
+
+
+    for(i=0; i != arr.length; i++){
         let taask = document.createElement("div");
         taask.className = "task";
+
+        if(arr[i].done == true){
+            taask.classList.add('backgroundGray')
+        }
 
         let ttittle = document.createElement("div");
         ttittle.className = "title";
         ttittle.innerText = arr[i].title;
         taask.appendChild(ttittle);
-        
+
         let descrip = document.createElement('div');
         descrip.className = 'descr';
         descrip.innerText = arr[i].desc;
         taask.appendChild(descrip);
-    
+
 
         let bott = document.createElement('div')
         bott.className = 'bott';
         taask.appendChild(bott);
-    
+
         let prior = document.createElement('div');
         prior.className = 'prio';
         prior.innerText = arr[i].prior;
         bott.appendChild(prior);
-    
+
         let menu = document.createElement('div');
         menu.className = 'menu';
-    
+
         let triTochki = document.createElement('div');
         triTochki.className = 'tri-tochki';
         triTochki.innerText = '...'
         menu.appendChild(triTochki);
-    
+
         let menUvIPaD = document.createElement('div')
         menUvIPaD.className = "menu-vipad";
         menu.appendChild(menUvIPaD);
-    
+
         let ArrOwUp = document.createElement('div');
         ArrOwUp.className = 'arrow-up'
         menUvIPaD.appendChild(ArrOwUp);
-    
-        let DoNe = document.createElement('div');
-        DoNe.className = 'done';
-        DoNe.innerText = 'done'
-        DoNe.dataset.index = arr[i]['index'];
-        menUvIPaD.appendChild(DoNe);
-    
+
+        ddone = document.createElement('div');
+        ddone.className = 'done';
+        ddone.innerText = 'done'
+        ddone.dataset.index = arr[i]['index'];
+        menUvIPaD.appendChild(ddone);
+
         let DeL = document.createElement('div');
         DeL.className = 'del';
         DeL.innerText = 'delete'
         DeL.dataset.index = arr[i]['index'];
         menUvIPaD.appendChild(DeL);
-    
+
         let EdIt = document.createElement('div');
         EdIt.className = 'edit';
         EdIt.innerText = 'edit';
         EdIt.dataset.index = arr[i]['index'];
         menUvIPaD.appendChild(EdIt)
-    
+
         bott.appendChild(menu)
         cs.appendChild(taask)
     }
     tasks.appendChild(cs)
-   
-    
+   addClicksToMenu();
+
 }
-
-
